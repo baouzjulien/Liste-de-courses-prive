06f3b7c5d1f0c8245bb0226be9dab11c5e6095d0
Julien Baouz <baouzjulien@gmail.com>
2025-12-28 20:21:30 +0100

    MAJ

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '1.0.5';
+const VERSION = '1.0.6';
 const CACHE_NAME = 'liste-courses-' + VERSION;
 
 const STATIC_ASSETS = [
@@ -11,12 +11,14 @@ const STATIC_ASSETS = [
   './icons/icon-512.png'
 ];
 
+// Installation : mise en cache des fichiers statiques
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
   );
 });
 
+// Activation : suppression des anciens caches
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
@@ -27,12 +29,14 @@ self.addEventListener('activate', event => {
   );
 });
 
+// Interception des requêtes : offline first
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
 
+// Forcer activation immédiate
 self.addEventListener('message', event => {
   if (event.data === 'skipWaiting') self.skipWaiting();
 });
